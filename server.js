5ba9ca2e4c98abf6f6cede7f2e0628395db86a30
suttiput <rockets2225@gmail.com>
2018-05-31 07:03:20 +0700

    server

--- server.js
+++ server.js
@@ -1,38 +1,134 @@
 const Hapi = require('hapi');
+const Boom = require('boom');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+
 
 const server = Hapi.server({
     port: 3000,
     host: 'localhost'
 });
+const launchServer = async function() {
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
+    const dbOpts = {
+        url: 'mongodb://localhost:8888/customers',
+        settings: {
+            poolSize: 10
+        },
+        decorate: true
+    };
 
-        return status;
-    }
-});
+    const swaggerOptions = {
+        info: {
+            title: 'BALLZA555+',
+            version: Pack.version,
+        },
+    };
+    await server.register([{
+            plugin: require('hapi-mongodb'),
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, h) => {
+            options: dbOpts
+        },
+        require('Inert'),
+        require('Vision'), {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
 
-        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
-    }
-});
+    ]);
 
-const init = async() => {
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            tags: ['api'],
+            async handler(request) {
+
+
+                const db = request.mongo.db;
+                const ObjectID = request.mongo.ObjectID;
+                console.log(request)
+                try {
+                    const result = await db.collection('customers').find().toArray();
+                    return result;
+                } catch (err) {
+                    throw Boom.internal('Internal MongoDB error', err);
+                }
+            }
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/add',
+        config: {
+            tags: ['api'],
+
+            async handler(request) {
+                const db = request.mongo.db;
+                console.log(request)
+                try {
+                    const result = await db.collection('customers').insert(request.payload);
+                    return result;
+                } catch (err) {
+                    throw Boom.internal('Internal MongoDB error', err);
+                }
+            }
+        },
+    });
+    server.route({
+        method: 'PUT',
+        path: '/put/{id}',
+        config: {
+            tags: ['api'],
+
+            async handler(request) {
+                const db = request.mongo.db;
+                const ObjectID = request.mongo.ObjectID;
+
+                console.log(request)
+                try {
+                    const result = await db.collection('customers').update({ _id: new ObjectID(request.params.id) }, request.payload);
+                    return result;
+                } catch (err) {
+                    throw Boom.internal('Internal MongoDB error', err);
+                }
+            }
+        },
+    });
+    server.route({
+        method: 'DELETE',
+        path: '/delete/{id}',
+        config: {
+            tags: ['api'],
+
+            async handler(request) {
+                const db = request.mongo.db;
+                const ObjectID = request.mongo.ObjectID;
+
+
+                console.log(request)
+                try {
+                    const result = await db.collection('customers').remove({ _id: new ObjectID(request.params.id) });
+                    return result;
+                } catch (err) {
+                    throw Boom.internal('Internal MongoDB error', err);
+                }
+            }
+        },
+    });
+
 
     await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+    console.log()
+    console.log(`ser ${server.info.uri}`);
+
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
+launchServer().catch((err) => {
+    console.error(err);
     process.exit(1);
 });
-
-init();
